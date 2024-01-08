814977a59c3b3927fe7538b2daf2e2e50a925019
jws2170 <mpart2077@gmail.com>
2024-01-08 14:09:40 +0900

    fix

--- MyPorject2024_01_08/Assets/Script/PlayerCon.cs
+++ MyPorject2024_01_08/Assets/Script/PlayerCon.cs
@@ -8,6 +8,7 @@ public class PlayerCon : MonoBehaviour
     public GameObject pivot;
     public Camera viewCamera;
     public Vector3 velocity;
+    public Rigidbody body;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +18,18 @@ public class PlayerCon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * movespeed;
+
+
         Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
         Vector3 targetPos = new Vector3(mousePos.x, transform.position.y, mousePos.z);
 
 
         pivot.transform.LookAt(targetPos, Vector3.up);
+    }
+
+    private void FixedUpdate()
+    {
+        body.MovePosition(body.position + velocity * Time.fixedDeltaTime);
     }
 }
